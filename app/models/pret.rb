2f073db14c9f2dc3ab7c53bd6e8ffd8a5c59350a
Tom Armitage <tom@infovore.org>
2015-02-26 17:38:09 +0000

    Fixing pret model.

--- app/models/pret.rb
+++ app/models/pret.rb
@@ -13,33 +13,38 @@ class Pret < ActiveRecord::Base
           #is_locked: false)
   #}
 
+  scope :open, -> {
+    where(has_opened: true)
+  }
+
   scope :by_proximity, ->(lat,lon) {
     srid = 4326
     order("ST_Distance(stations.latlon, ST_GeomFromText('POINT (#{lon} #{lat})', #{srid}))")
   }
 
-  #def to_geo_json
-    #{
-      #:type => "Feature",
-      #:geometry => {
-        #:type => "Point",
-        #:coordinates => [lon,lat]
-      #},
-      #:properties => {
-        #:name => name,
-        #:is_installed => is_installed,
-        #:is_locked => is_locked,
-        #:install_date => install_date,
-        #:removal_date => removal_date,
-        #:is_temporary => is_temporary,
-        #:number_bikes => number_bikes,
-        #:number_docks => number_docks,
-        #:number_empty_docks => number_empty_docks,
-      #}
-    #}
-  #end
+  def to_geo_json
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [lon,lat]
+      },
+      properties: {
+        name: name,
+        phone_number: phone_number,
+        seating: seating,
+        has_toilets: has_toilets,
+        has_wheelchair_access: has_wheelchair_access,
+        has_wifi: has_wifi,
+        has_opened: has_opened,
+        address: address,
+        directions: directions,
+        pret_number: pret_number
+      }
+    }
+  end
 
-  def distance_and_arc_from_lonlat_to_station(origin_lon,origin_lat,offset_bearing=nil)
+  def distance_and_arc_from_lonlat_to_pret(origin_lon,origin_lat,offset_bearing=nil)
     deg = Pret.connection.select_all("select degrees( ST_Azimuth(ST_Point(#{origin_lon},#{origin_lat}), ST_Point(#{lon},#{lat})))").rows[0][0] # first field of first row
 
     distance = Pret.connection.select_all("SELECT round(CAST(ST_Distance_Sphere(ST_Point(#{origin_lon},#{origin_lat}), ST_Point(#{lon},#{lat})) As numeric),2) As dist_meters").rows[0][0]
@@ -83,26 +88,26 @@ class Pret < ActiveRecord::Base
   end
 
   def self.collection_to_feature_collection(prets)
-    { :type => "FeatureCollection",
-      :features => prets.map {|s| s.to_geo_json}
+    {
+      type: "FeatureCollection",
+      features: prets.map(&:to_geo_json)
     }.to_json
   end
 
   # TODO : nearest open
 
-  # TODO: ingest routinesk
   def self.create_or_update_from_hashie(pret_hashie)
     pret = Pret.find_or_initialize_by(:pret_number => pret_hashie.number.to_i)
     pret.update(
 
       :name => pret_hashie.details.name,
-      :latlon => pret_hashie.details.phone_number,
       :latlon => "POINT(#{pret_hashie.location.longitude} #{pret_hashie.location.latitude})",
       :phone_number => pret_hashie.details.phone_number,
       :seating => pret_hashie.facilities.seating,
       :has_toilets => pret_hashie.facilities.has_toilets.eql?('Yes'),
       :has_wheelchair_access => pret_hashie.facilities.wheelchair_access,
       :has_wifi => pret_hashie.facilities.wifi,
+      :has_opened => pret_hashie.details.has_opened,
       :address => pret_hashie.location.address,
       :directions => pret_hashie.location.getting_there,
       :pret_number => pret_hashie.number.to_i,
