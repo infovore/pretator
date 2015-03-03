f7d9d91cd80d57779b64c743ab52da00dc444a85
Tom Armitage <tom@infovore.org>
2015-03-03 10:08:36 +0000

    Secrets should be kept out of the repo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :keep_releases, 5
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
